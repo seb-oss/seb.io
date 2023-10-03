14ff378c9b9a046cc71f743e6ff7072a014868fe
astrit <am@astrit.co>
2023-10-03 15:37:51 +0200

    Msg → Tue Oct  3 15:37:51 CEST 2023

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export",
+};
 
 module.exports = withContentlayer(nextConfig);
