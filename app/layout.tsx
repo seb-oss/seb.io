34091560f0eb748ef1f240a95ba6fb141e8e2b81
astrit <am@astrit.co>
2023-10-04 09:37:07 +0200

    Init opengraph image generator

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,19 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://seb.io"),
   title: "GDS",
   description: "Green Design System",
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "de-DE": "/de-DE",
+    },
+  },
+  openGraph: {
+    images: "/og-image.png",
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +27,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:alt" content="Green Design System" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
