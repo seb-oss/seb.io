c0e77de602eaced6c711a316ea7d23fe159b4473
astrit <am@astrit.co>
2023-10-30 19:41:40 +0100

    Msg → Mon Oct 30 19:41:40 CET 2023

--- utils/theme/provider.tsx
+++ utils/theme/provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect, useRef } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { Toaster } from "sonner";
@@ -13,6 +13,14 @@ export const ThemeProviderContext = createContext({
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
+  // const buttonRef = useRef<HTMLButtonElement>(null);
+  // const [gdsColor, setGdsColor] = useState<string | null>(null);
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [hoverStyles, setHoverStyles] = useState<CSSStyleDeclaration | null>(
+    null
+  );
+
   const toggleCmd = () => {
     setIsOpen((prevOpen) => !prevOpen);
   };
@@ -21,10 +29,48 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     toggleCmd,
   };
 
+  const handleMouseEnter = () => {
+    const styles = getComputedStyle(buttonRef.current as Element);
+    const color = styles.getPropertyValue("--gds-color");
+    console.log(color);
+    console.log(styles);
+
+    // const gdsProperties = Object.keys(styles).filter((name) =>
+    //   name.includes("--gds")
+    // );
+    // const gdsValues = gdsProperties.map((name) =>
+    //   styles.getPropertyValue(name)
+    // );
+    // console.log(gdsValues);
+
+    // const buttonRefCurrent = buttonRef.current;
+    // if (!buttonRefCurrent) return;
+    // const computedStyles = window.getComputedStyle(buttonRefCurrent);
+    // const styleDeclaration = buttonRefCurrent.style;
+    // console.log(styleDeclaration);
+    // const cssProperties = new Set();
+    // for (let i = 0; i < computedStyles.length; i++) {
+    //   const propertyName = computedStyles[i];
+    //   if (propertyName.startsWith("--gds")) {
+    //     cssProperties.add(propertyName);
+    //   }
+    // }
+    // for (const propertyName of cssProperties) {
+    //   const propertyValue = styleDeclaration.getPropertyValue(propertyName);
+    //   console.log(propertyName, propertyValue);
+    // }
+    // console.log(color);
+  };
+
   return (
     <NextThemesProvider {...props}>
       <ThemeProviderContext.Provider value={themeProviderValue}>
+        {/* <button ref={buttonRef} onMouseEnter={handleMouseEnter}>
+          Hover me
+        </button> */}
+
         {children}
+
         <Toaster
           richColors
           theme="dark"
