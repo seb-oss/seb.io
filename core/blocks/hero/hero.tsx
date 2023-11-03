733689619d505efa7372b9157cc5209f790454ec
astrit <am@astrit.co>
2023-11-03 11:30:51 +0100

    Hero styling

--- core/blocks/hero/hero.tsx
+++ core/blocks/hero/hero.tsx
@@ -1,10 +1,12 @@
 "use client"
 
+import Link from "next/link"
+
 import "./style.css"
 
 export default function Hero() {
   return (
-    <div>
+    <div className="hero">
       <h1>
         Design and build beautiful, usable products with Green Design System
       </h1>
@@ -12,7 +14,6 @@ export default function Hero() {
         Green Design System is the latest version of SEB’s open-source design
         system. Welcome to a new era of digital banking.
       </p>
-      <button>Ask question</button>
     </div>
   )
 }
