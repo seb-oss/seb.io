20a51fcca5bdd2879548ec3578e57cfac7476047
astrit <am@astrit.co>
2023-11-20 10:40:03 +0100

    Fix the metadata image

--- utils/metadata/image.tsx
+++ utils/metadata/image.tsx
@@ -1,5 +1,6 @@
-import { ImageResponse } from "next/og"
+// import { ImageResponse } from "next/og"
 import { NextRequest } from "next/server"
+import { ImageResponse } from "@vercel/og"
 
 export const runtime = "edge"
 
