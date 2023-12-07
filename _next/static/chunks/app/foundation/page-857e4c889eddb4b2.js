5bb3f6fc15b978d509d37a4a345fc611651cb7c6
astrit <am@astrit.co>
2023-12-07 14:38:01 +0000

    Deploying to gh-pages from @ sebgroup/seb.io@df5d66c40343adaf17fe4790e7cd385d1633527e 🚀

---
+++ _next/static/chunks/app/foundation/page-857e4c889eddb4b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[310,540,404,431,831],{5611:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=5611)}),_N_E=n.O()}]);
