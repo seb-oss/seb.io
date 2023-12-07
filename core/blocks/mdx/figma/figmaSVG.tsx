46ecbe2fb9fd3f52ab7646df15886fb852d2f31e
Joacim Magnusson <joacim@isogram.se>
2023-12-07 14:54:36 +0100

    ci: update gh-pages deploy step

--- core/blocks/mdx/figma/figmaSVG.tsx
+++ core/blocks/mdx/figma/figmaSVG.tsx
@@ -2,19 +2,13 @@
 
 import * as React from "react"
 import { usePathname } from "next/navigation"
-import { allComponents, Component } from "@/.contentlayer/generated"
+import { allComponents } from "@/.contentlayer/generated"
 import Pattern from "@/core/blocks/pattern/pattern"
 
 interface FigmaSVGProps {
   caption?: string
   node?: string
   height?: string
-}
-
-export const generateStaticParams = (): any => {
-  return allComponents.map((component) => ({
-    slug: component.url_path.replace("/component/", ""),
-  }))
 }
 
 export default function FigmaSVG({ caption, node, height }: FigmaSVGProps) {
