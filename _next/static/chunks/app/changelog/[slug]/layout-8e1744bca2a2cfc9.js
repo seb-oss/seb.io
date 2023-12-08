3e50639a8e2c9b1d5bd1f6e8a533fb01a24bc554
astrit <am@astrit.co>
2023-12-08 11:18:23 +0000

    Deploying to gh-pages from @ sebgroup/seb.io@89e0359af96efe1523a37468e66adc539fb21d35 🚀

--- _next/static/chunks/app/changelog/[slug]/layout-8e1744bca2a2cfc9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977,143,841,50,286,768,605,686,989,244,429,82],{3521:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/components/layout-3321ce71799d03a4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[686,143,841,50,286,768,989,244,429,605,82,977],{3521:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
