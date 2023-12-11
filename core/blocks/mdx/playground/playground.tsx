8b6e29b38231fa502afa52d9efe44f318abded5e
Vilhelm Sjölander <vilhelm.sjolander@gmail.com>
2023-12-11 22:01:24 +0100

    fix(playground): load correctly when switching pages

--- core/blocks/mdx/playground/playground.tsx
+++ core/blocks/mdx/playground/playground.tsx
@@ -1,3 +1,5 @@
+import { use, useEffect, useRef } from "react"
+
 import "./styles.css"
 
 declare global {
@@ -26,13 +28,30 @@ declare global {
 }
 
 function Playground(props: any) {
+  const projectRef = useRef<HTMLDivElement>(null)
+  const previewRef = useRef<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
+        project: string | HTMLDivElement
+      },
+      HTMLElement
+    >
+  >(null)
+
+  useEffect(() => {
+    if (previewRef.current && projectRef.current) {
+      previewRef.current.project = projectRef.current
+    }
+  })
+
   return (
     <>
       <playground-project
         id="story-project"
+        ref={projectRef}
         project-src={`/stories/${props.component}/project.json`}
       ></playground-project>
-      <playground-preview project="story-project"> </playground-preview>
+      <playground-preview ref={previewRef}></playground-preview>
     </>
   )
 }
