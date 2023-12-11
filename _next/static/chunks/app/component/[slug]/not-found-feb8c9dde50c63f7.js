ce32ba434a86b17263c42b2e4696b2b0fb754408
astrit <am@astrit.co>
2023-12-11 08:41:31 +0000

    Deploying to gh-pages from @ sebgroup/seb.io@2992ab51f27fb83c2c2536ba8c11ec9c62100b97 🚀

--- _next/static/chunks/app/component/[slug]/not-found-feb8c9dde50c63f7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[143,768,841,50,286,605,989,686,244,429,977,82],{3521:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
