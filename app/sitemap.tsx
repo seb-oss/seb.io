3320894a2a2c3b3d42205f79237a4a31edb854e8
astrit <am@astrit.co>
2023-12-13 13:24:41 +0100

    Add sitemap and robots.txt

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -15,7 +15,43 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: "https://seb.io/component/button",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://seb.io/component/input",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://seb.io/foundation",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://seb.io/foundation/accessibility",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://seb.io/about",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://seb.io/changelog",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: "https://seb.io/status",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
