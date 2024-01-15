2bef91b532eacf6ae54c7d6f3077875e47e49eed
astrit <am@astrit.co>
2024-01-15 12:09:04 +0100

    Msg → Mon Jan 15 12:09:04 CET 2024

---
+++ utils/auth/auth.ts
@@ -0,0 +1,19 @@
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+});
