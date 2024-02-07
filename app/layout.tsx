d7d7f29889d0806b39247c613366be175fe52c66
Conny Andersson <conny.x.andersson@seb.se>
2024-02-07 18:37:23 +0100

    Revert "Merge branch 'main' into Conny-buttons"
    This reverts commit fe2f7defd6bbf320896ba50abc07bad89e077fec, reversing
    changes made to 292830df3e338656ddc1c8c1ab6238602f2f172d.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next"
-import Script from "next/script"
 import Alert from "@/core/blocks/alert/aler"
 import Consent from "@/core/blocks/consent/consent"
 import Footer from "@/core/blocks/footer"
@@ -35,18 +34,6 @@ export default function RootLayout({
           <Consent />
           <Footer />
         </ThemeProvider>
-
-        <Script id="data-layer">
-          {`window["dataLayer"] = {
-            "pageName":"seb.io",
-            "pagetype":"StandardPage",
-            "language":"en",
-            "environment":"prod",
-            "project":"green",
-            "website":"seb.io",
-          };`}
-        </Script>
-        <Script src="https://content.seb.se/dsc/da/launch/public/30e54a9d6c99/f9d07ef22744/launch-89d260357525.min.js" />
       </body>
     </html>
   )
