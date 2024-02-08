4b3bca0fef5865db87454a923f7650d0abe30ed8
astrit <am@astrit.co>
2024-02-08 14:26:01 +0100

    Msg → Thu Feb  8 14:26:01 CET 2024

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             "environment":"prod",
             "project":"green",
             "website":"seb.io",
-          };console.log(window["dataLayer"]);`}
+          };`}
         </Script>
       </body>
     </html>
