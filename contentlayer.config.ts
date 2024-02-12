4a7830577aa10518a5fe1139228f0f1f00f4acae
vsjolander <vilhelm.sjolander@gmail.com>
2024-02-12 14:56:23 +0100

    added defer

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,36 +1,39 @@
-import { makeSource } from '@contentlayer/source-files'
-import remarkGfm from 'remark-gfm';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import highlight from 'rehype-highlight'
-import rehypePrettyCode from 'rehype-pretty-code';
-import { contentDirPath } from './utils/content/utils'
-import * as documentTypes from './utils/content/types'
+import { makeSource } from "@contentlayer/source-files"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import highlight from "rehype-highlight"
+import rehypePrettyCode from "rehype-pretty-code"
+import rehypeSlug from "rehype-slug"
+import remarkGfm from "remark-gfm"
+
+import * as documentTypes from "./utils/content/types"
+import { contentDirPath } from "./utils/content/utils"
 
 export default makeSource({
   contentDirPath,
   documentTypes,
-  // mdx: { rehypePlugins: [highlight] },
-
   mdx: {
     remarkPlugins: [remarkGfm],
-    
+
     rehypePlugins: [
       rehypeSlug,
       [
         rehypePrettyCode,
         {
-          theme: 'one-dark-pro',
-          onVisitLine(node: { children: string | any[]; }) {
+          theme: "one-dark-pro",
+          onVisitLine(node: { children: string | any[] }) {
             if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: ' ' }];
+              node.children = [{ type: "text", value: " " }]
             }
           },
-          onVisitHighlightedLine(node: { properties: { className: string[]; }; }) {
-            node.properties.className.push('line--highlighted');
+          onVisitHighlightedLine(node: {
+            properties: { className: string[] }
+          }) {
+            node.properties.className.push("line--highlighted")
           },
-          onVisitHighlightedWord(node: { properties: { className: string[]; }; }) {
-            node.properties.className = ['word--highlighted'];
+          onVisitHighlightedWord(node: {
+            properties: { className: string[] }
+          }) {
+            node.properties.className = ["word--highlighted"]
           },
         },
       ],
@@ -39,7 +42,7 @@ export default makeSource({
         {
           behavior: "wrap",
           properties: {
-            className: ['anchor'],
+            className: ["anchor"],
           },
         },
       ],
