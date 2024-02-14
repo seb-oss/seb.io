093a13d36eaeb38195ceac4d023c05307a0bc251
astrit <am@astrit.co>
2024-02-14 17:30:36 +0100

    Grid area sample

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -1,7 +1,25 @@
 import Grid from "@/core/blocks/grid/grid"
 
 export default function GridPage() {
-    return (
-        <Grid />
-    )
+  return (
+    <div style={{ flexDirection: "column" }}>
+      <Grid />
+
+      <br />
+      <div className="app">
+        <div className="app-alert">alert</div>
+        <div className="app-header">header</div>
+        <div className="app-layout">
+          <div className="sidebar">sidebar</div>
+          <div className="content">
+            <div className="hero">Hero</div>
+            <div className="secondary">Secondary</div>
+            <div className="content">Content</div>
+            <div className="toc">Toc</div>
+          </div>
+        </div>
+        <div className="app-footer">footer</div>
+      </div>
+    </div>
+  )
 }
