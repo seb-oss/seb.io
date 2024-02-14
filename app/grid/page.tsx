9da516bab1178052d906ab62ac3ef82d99bf2af9
astrit <am@astrit.co>
2024-02-14 23:01:17 +0100

    Start using the grid areas

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -2,23 +2,23 @@ import Grid from "@/core/blocks/grid/grid"
 
 export default function GridPage() {
   return (
-    <div style={{ flexDirection: "column" }}>
-      <Grid />
-
-      <br />
-      <div className="app">
-        <div className="app-alert">alert</div>
-        <div className="app-header">header</div>
-        <div className="app-layout">
-          <div className="sidebar">sidebar</div>
-          <div className="content">
-            <div className="hero">Hero</div>
-            <div className="secondary">Secondary</div>
-            <div className="content">Content</div>
-            <div className="toc">Toc</div>
+    <div style={{ flexDirection: "column", width: "100%" }}>
+      <div className="grid-test">
+        <Grid />
+        <div className="app">
+          <div className="app-alert">alert</div>
+          <div className="app-header">header</div>
+          <div className="app-layout">
+            <div className="sidebar">sidebar</div>
+            <div className="content">
+              <div className="hero">Hero</div>
+              <div className="secondary">Secondary</div>
+              <div className="content">Content</div>
+              <div className="toc">Toc</div>
+            </div>
           </div>
+          <div className="app-footer">footer</div>
         </div>
-        <div className="app-footer">footer</div>
       </div>
     </div>
   )
