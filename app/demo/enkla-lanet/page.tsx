fe6e1f76cff445ce63299cf0b0b23a474cc44094
astrit <am@astrit.co>
2024-04-09 10:31:54 +0200

    Msg → Tue Apr  9 10:31:54 CEST 2024

--- app/demo/enkla-lanet/page.tsx
+++ app/demo/enkla-lanet/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import Image from "next/image"
 import Grid from "@/grid/grid"
 
+import "./style.css"
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://seb.io/demo/enkla-lanet"),
   title: "Enkla Lanet — Green Design System",
@@ -9,24 +11,27 @@ export const metadata: Metadata = {
 
 export default function EnklaLanet() {
   return (
-    <div>
+    <div className="enkla">
       <h1>Demo: Enkla lanet</h1>
       <p>WARNING: This is a demo only!</p>
 
-      <Grid columns={2}>
-        <div>
-          <Image
-            src={`https://source.unsplash.com/user/wsanter`}
-            alt="Placeholder"
-            width={500}
-            height={500}
-          />
-        </div>
-        <div>
-          <form action="">
-            <input type="text" placeholder="Test" />
-          </form>
-        </div>
+      <Grid
+        columns={2}
+        gapBlock="large"
+        gapInline="large"
+        paddingBlock="medium"
+        paddingInline="medium"
+      >
+        <Image
+          src={`https://source.unsplash.com/user/cdombaxi_6`}
+          alt="Placeholder"
+          width={500}
+          height={1200}
+          className="figure"
+        />
+        <form action="">
+          <input type="text" placeholder="Test" />
+        </form>
       </Grid>
     </div>
   )
